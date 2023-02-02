7b02a1fe84a4cebffe30853f9d5a93aa682d2c2d
Prabhat Ranjan <prabhat0206@hotmail.com>
2023-02-02 12:05:56 +0530

    ftp-server

---
+++ index.ts
@@ -0,0 +1,25 @@
+import FtpSrv from "ftp-srv";
+import { config } from "dotenv";
+
+config();
+
+const ftpServer = new FtpSrv({
+  url: "ftp://0.0.0.0:21",
+});
+
+ftpServer.on("login", (data, resolve, reject) => {
+  const username = data.username;
+  const password = data.password;
+
+  if (username === "user" && password === "pass") {
+    resolve({
+      root: "./public",
+    });
+  } else {
+    reject("Invalid username or password" as any);
+  }
+});
+
+ftpServer.listen().then(() => {
+  console.log("Listening on port 21");
+});
